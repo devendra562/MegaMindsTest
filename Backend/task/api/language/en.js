f4ebecd63535fbb2d1b92391254874d6b7fae11d
Devendra Umaretiya <devendraumaretiya2002@gmail.com>
2025-07-22 15:09:39 +0530

    few chnages update

--- Backend/task/api/language/en.js
+++ Backend/task/api/language/en.js
@@ -2,8 +2,7 @@ const messages = {
     "rest_keywords_user_something_wrong": "Something went wrong",
     "rest_keywords_user_listing_succ": "User listing successful",
     "rest_keywords_not_found": "Entry not found",
-    "rest_keywords_data_updated": "Data updated successfully",
-    "rest_keywords_new_key": "Your new value"
+    "rest_keywords_data_updated": "Data updated successfully"
 };
 
 module.exports = messages;
