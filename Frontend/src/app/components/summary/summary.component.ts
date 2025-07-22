3d461d1c403ef4701fd1e049179966d8d8ef1bc7
Devendra Umaretiya <devendraumaretiya2002@gmail.com>
2025-07-22 14:22:17 +0530

    final updated task

--- Frontend/src/app/components/summary/summary.component.ts
+++ Frontend/src/app/components/summary/summary.component.ts
@@ -1,8 +1,6 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { DataService } from '../../services/data.service';
-import { response } from 'express';
-import { log } from 'console';
 
 @Component({
   selector: 'app-summary',
@@ -11,7 +9,7 @@ import { log } from 'console';
   templateUrl: './summary.component.html',
   styleUrl: './summary.component.css'
 })
-export class SummaryComponent {
+export class SummaryComponent implements OnInit {
    datas: any[] = [];
 
    constructor(private dataService: DataService){}
@@ -19,6 +17,12 @@ export class SummaryComponent {
    ngOnInit(): void {
     this.dataService.getData().subscribe((response: any) => {
       this.datas = response.data.Datas;
-    })
+    });
+   }
+
+   // Helper method to get property value by label
+   getPropertyValue(item: any, propertyLabel: string): string {
+    const property = item.Properties?.find((prop: any) => prop.Label === propertyLabel);
+    return property ? property.Value : '';
    }
 }
